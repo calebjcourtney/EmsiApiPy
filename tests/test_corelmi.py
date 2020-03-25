b35949041ed8152c3c3dcfa445b4740ba358ca5e
Caleb Courtney <brokenfoot@rocketmail.com>
2020-03-24 19:44:30 -0700

    updating the tests

--- tests/test_corelmi.py
+++ tests/test_corelmi.py
@@ -13,70 +13,110 @@ from apis.coreLmi import CoreLMIConnection
 lmi_conn = CoreLMIConnection()
 
 
-def test_get_meta():
-    response = lmi_conn.get_meta()
-    assert response != {}
+# def test_get_meta():
+#     response = lmi_conn.get_meta()
+#     assert response != {}
 
 
-def test_get_meta_dataset():
-    response = lmi_conn.get_meta_dataset(dataset = "emsi.us.occupation", datarun = 'latest')
-    assert response != {}
+# def test_get_meta_dataset():
+#     response = lmi_conn.get_meta_dataset(dataset = "emsi.us.occupation", datarun = 'latest')
+#     assert response != {}
 
 
-def test_get_meta_dataset_dimension():
-    response = lmi_conn.get_meta_dataset_dimension("emsi.us.occupation", "Occupation", datarun = 'latest')
-    for column in ['name', 'hierarchy']:
-        assert column in response
+# def test_get_meta_dataset_dimension():
+#     response = lmi_conn.get_meta_dataset_dimension("emsi.us.occupation", "Occupation", datarun = 'latest')
+#     for column in ['name', 'hierarchy']:
+#         assert column in response
 
 
-def test_post_retrieve_data():
-    payload = {
-        "metrics": [
-            {
-                "name": "Jobs.2019"
-            }
-        ],
-        "constraints": [
-            {
-                "dimensionName": "Area",
-                "map": {"US": [0]}
-            }
-        ]
-    }
-    response = lmi_conn.post_retrieve_data("emsi.us.occupation", payload, datarun = 'latest')
+# def test_post_retrieve_data():
+#     payload = {
+#         "metrics": [
+#             {
+#                 "name": "Jobs.2019"
+#             }
+#         ],
+#         "constraints": [
+#             {
+#                 "dimensionName": "Area",
+#                 "map": {"US": [0]}
+#             }
+#         ]
+#     }
+#     response = lmi_conn.post_retrieve_data("emsi.us.occupation", payload, datarun = 'latest')
 
-    for key in ['data', 'errors', 'timings', 'totalRows']:
-        assert key in response
+#     for key in ['data', 'errors', 'timings', 'totalRows']:
+#         assert key in response
 
-    assert response['totalRows'] == 1
-
-
-def test_get_dimension_hierarchy_df():
-    df = lmi_conn.get_dimension_hierarchy_df('emsi.us.occupation', 'Occupation', datarun = 'latest')
-
-    assert not df.empty
-    for column in ['name', 'orderid', 'parent', 'descr', 'child', 'level_name']:
-        assert column in df.columns
+#     assert response['totalRows'] == 1
 
 
-def test_post_retrieve_df():
-    payload = {
-        "metrics": [
-            {
-                "name": "Jobs.2019"
-            }
-        ],
-        "constraints": [
-            {
-                "dimensionName": "Area",
-                "map": {"US": [0]}
-            }
-        ]
-    }
+# def test_get_dimension_hierarchy_df():
+#     df = lmi_conn.get_dimension_hierarchy_df('emsi.us.occupation', 'Occupation', datarun = 'latest')
 
-    df = lmi_conn.post_retrieve_df("emsi.us.occupation", payload, datarun = 'latest')
+#     assert not df.empty
+#     for column in ['name', 'orderid', 'parent', 'descr', 'child', 'level_name']:
+#         assert column in df.columns
 
-    assert not df.empty
-    assert len(df) == 1
-    for column in ['Area', 'Jobs.2019']:
-        column in df.columns
+
+# def test_post_retrieve_df():
+#     payload = {
+#         "metrics": [
+#             {
+#                 "name": "Jobs.2019"
+#             }
+#         ],
+#         "constraints": [
+#             {
+#                 "dimensionName": "Area",
+#                 "map": {"US": [0]}
+#             }
+#         ]
+#     }
+
+#     df = lmi_conn.post_retrieve_df("emsi.us.occupation", payload, datarun = 'latest')
+
+#     assert not df.empty
+#     assert len(df) == 1
+#     for column in ['Area', 'Jobs.2019']:
+#         column in df.columns
+
+
+def test_download_data():
+    limit = 300
+    while limit > 1:
+        payload = {
+            "metrics": [
+                {
+                    "name": "Jobs.2019"
+                }
+            ],
+            "constraints": [
+                {
+                    "dimensionName": "Area",
+                    "map": {
+                        "US": [
+                            0
+                        ]
+                    }
+                }
+            ]
+        }
+
+        response = lmi_conn.download_data("emsi.us.industry", payload)
+
+        limit = int(response.headers['X-Rate-Limit-Remaining'])
+
+        if limit == 1:
+            break
+
+    response = lmi_conn.download_data("emsi.us.industry", payload)
+    print(response.text)
+
+    response = lmi_conn.download_data("emsi.us.industry", payload)
+    print(response.text)
+
+    response = lmi_conn.download_data("emsi.us.industry", payload)
+    print(response.text)
+
+    assert False
