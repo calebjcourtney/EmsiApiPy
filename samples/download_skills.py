4043fbd43d607e77aabb54cbb10fda12c87c68cb
Caleb Courtney <brokenfoot@rocketmail.com>
2020-11-19 08:45:20 -0800

    updating the script for exporting the skills library

--- samples/download_skills.py
+++ samples/download_skills.py
@@ -5,10 +5,17 @@ import pandas as pd
 conn = EmsiApiPy.SkillsClassificationConnection()
 
 # download all the skills
-data = conn.get_list_all_skills()
+data = conn.get_list_all_skills()['data']
 
 # load into pandas
-df = pd.DataFrame(data['data'])
+df = pd.DataFrame(
+    {
+        "id": [record['id'] for record in data],
+        "infoUrl": [record['infoUrl'] for record in data],
+        "name": [record['name'] for record in data],
+        "type": [record['type']['name'] for record in data]
+    }
+)
 
 # export
 writer = pd.ExcelWriter("skills_list.xlsx")
